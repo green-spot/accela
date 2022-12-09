97fd82027a02aba7152d39f13b8f8bf671c94276
green spot <in.green.spot@gmail.com>
2022-12-09 10:05:19 +0900

    fix

--- accela/index.php
+++ accela/index.php
@@ -2,14 +2,14 @@
 
 namespace Accela;
 
+if(file_exists(__DIR__ . "/env.php")){
+  require_once __DIR__ . "/env.php";
+}
+
 require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/app/page-init.php";
 
 define("ROOT_DIR", __DIR__);
 define("APP_DIR", ROOT_DIR . "/app");
-
-if(file_exists(__DIR__ . "/env.php")){
-  require_once __DIR__ . "/env.php";
-}
 
 Accela::route(isset($_SERVER["PATH_INFO"]) ? $_SERVER["PATH_INFO"] : "/");
