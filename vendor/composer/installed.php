f5fa51f9e522f3c7340720f4e91a8150fe4e7a8e
green spot <in.green.spot@gmail.com>
2022-12-12 22:49:10 +0900

    fix

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7d3eabcc1cd1554bdacb49b96556ce54880bc3da',
+        'name' => 'green-spot/accela',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'green-spot/accela' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7d3eabcc1cd1554bdacb49b96556ce54880bc3da',
+            'dev_requirement' => false,
+        ),
+    ),
+);
