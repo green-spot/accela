447a81271aa38c59496bd09346694f3a73ef41e0
green-spot <in.green.spot@gmail.com>
2025-04-10 18:11:03 +0900

    fix

--- src/views/template.php
+++ src/views/template.php
@@ -10,6 +10,6 @@
 <body>
 <div id="accela"></div>
 <script>const ACCELA = <?php echo json_encode(Accela\getInitialData($page)); ?>; ACCELA.modules = {};</script>
-<script src="/assets/js/accela.js?__t=<?php echo Accela\getUtime(); ?>" defer></script>
+<script src="/assets/js/accela.js?__t=<?php echo Accela\getUtime(); ?>" type="module"></script>
 </body>
 </html>
