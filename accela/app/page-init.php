cf7fc4d0ef285a2f2708620e3a523abb0a4f5f6f
green-spot <in.green.spot@gmail.com>
2025-06-24 15:18:24 +0900

    fix

--- accela/app/page-init.php
+++ accela/app/page-init.php
@@ -5,6 +5,9 @@ namespace Accela;
 // lang
 define("HTML_LANG", "ja");
 
+// for sitemap.xml
+define("ROOT_URL", "https://example.com");
+
 // seconds for Cache-Control
 // define("SERVER_LOAD_INTERVAL", 60);
 
